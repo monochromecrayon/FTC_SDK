4cc7fa82c3dd511590d68af4b84e72942610af95
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-01-30 14:49:55 -0600

    MAKING CHANGES *DUN DUN DUn DUn DUN :rocket:

---
+++ ftc_app-master/FtcRobotController/src/main/java/Constants.java
@@ -0,0 +1,9 @@
+package com.qualcomm.ftcrobotcontroller;
+
+public class Constants {
+    // public static final <type> <name> = <value>
+    public final static int ENCODER_CPR = 1120;
+    public final static double GEAR_RATIO = 1.5;
+    public final static int WHEEL_DIAMETER = 6;
+    public final static int DISTANCE = 72;
+}
